9166fd85de6dc878c418d7b70d126b44730e62b9
juandxce <juandxce@gmail.com>
2021-01-26 14:27:09 -0600

    Updated config for deployments for https

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-	apiurl: process.env.NEXT_PUBLIC_API_URL || 'localhost:3000'
+	apiurl: process.env.NEXT_PUBLIC_API_URL ? `https://${process.env.NEXT_PUBLIC_API_URL}` : 'http://localhost:3000'
 }
 
 export default config
